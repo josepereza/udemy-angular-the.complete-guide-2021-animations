caf481963da64fe4006f0b27ee1d62f3b86636cf
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-20 14:07:21 -0700

    feat: Lesson 406 Switching between Sates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor: 'blue',
+        'background-color': 'blue',
         transform: 'translateX(100px)'
       }))
     ])
@@ -20,6 +20,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
+
+  onShrink() {}
 
     onAdd(item) {
       this.list.push(item);
